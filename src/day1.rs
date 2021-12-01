25a4b998fe32bb279bc6cb2edc465a88ab2e2e31
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-12-01 13:52:27 +0100

    Improve count

--- src/day1.rs
+++ src/day1.rs
@@ -16,35 +16,30 @@ impl ParseInput<Day1> for Aoc2021 {
     }
 }
 
-impl Solution<Day1> for Aoc2021 {
-    type Part1Output = u32;
-    type Part2Output = u32;
-
-    fn part1(input: &Vec<u32>) -> u32 {
-        let mut counter = 0;
-        for w in input.windows(2) {
-            let (a, b) = (w[0], w[1]);
-            if a < b {
-                counter += 1;
-            }
+fn count_increasing_windows(values: &[u32]) -> usize {
+    let mut counter = 0;
+    for w in values.windows(2) {
+        let (a, b) = (w[0], w[1]);
+        if a < b {
+            counter += 1;
         }
-        counter
+    }
+    counter
+}
+
+impl Solution<Day1> for Aoc2021 {
+    type Part1Output = usize;
+    type Part2Output = usize;
+
+    fn part1(input: &Vec<u32>) -> usize {
+        count_increasing_windows(input)
     }
 
-    fn part2(input: &Vec<u32>) -> u32 {
-        let mut counter = 0;
-
-        let windows: Vec<u32> = input
+    fn part2(input: &Vec<u32>) -> usize {
+        let sums: Vec<u32> = input
             .windows(3)
             .map(|w| w.into_iter().copied().reduce(|a, b| a + b).unwrap())
             .collect();
-
-        for w in windows.windows(2) {
-            let (a, b) = (w[0], w[1]);
-            if a < b {
-                counter += 1;
-            }
-        }
-        counter
+        count_increasing_windows(&sums)
     }
 }
