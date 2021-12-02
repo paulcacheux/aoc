6f3e72d0a8a9f587e85750eaab157493644bff6c
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-12-02 09:49:58 +0100

    Improve day1 part2 (O(n) solution)

--- src/day1.rs
+++ src/day1.rs
@@ -2,7 +2,6 @@ use crate::aoc::Aoc2021;
 use advent_of_code_traits::days::Day1;
 use advent_of_code_traits::ParseInput;
 use advent_of_code_traits::Solution;
-use itertools::Itertools;
 
 impl ParseInput<Day1> for Aoc2021 {
     type Parsed = Vec<u32>;
@@ -17,30 +16,28 @@ impl ParseInput<Day1> for Aoc2021 {
     }
 }
 
-fn count_increasing_windows(values: &[u32]) -> usize {
-    let mut counter = 0;
-    for w in values.windows(2) {
-        let (a, b) = (w[0], w[1]);
-        if a < b {
-            counter += 1;
-        }
-    }
-    counter
-}
-
 impl Solution<Day1> for Aoc2021 {
     type Part1Output = usize;
     type Part2Output = usize;
 
     fn part1(input: &Vec<u32>) -> usize {
-        count_increasing_windows(input)
+        let mut counter = 0;
+        for w in input.windows(2) {
+            let (a, b) = (w[0], w[1]);
+            if a < b {
+                counter += 1;
+            }
+        }
+        counter
     }
 
     fn part2(input: &Vec<u32>) -> usize {
-        let sums: Vec<u32> = input
-            .windows(3)
-            .map(|w| w.iter().copied().sum1().unwrap())
-            .collect();
-        count_increasing_windows(&sums)
+        let mut counter = 0;
+        for w in input.windows(4) {
+            if w[0] < w[3] {
+                counter += 1;
+            }
+        }
+        counter
     }
 }
