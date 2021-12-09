f7c5271759259cead8364b549c298399fa51c6f3
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-12-09 13:25:35 +0100

    Day 9

---
+++ src/aoc2021/day9.rs
@@ -0,0 +1,151 @@
+use crate::aoc2021::Aoc2021;
+use advent_of_code_traits::days::Day9;
+use advent_of_code_traits::ParseInput;
+use advent_of_code_traits::Solution;
+
+#[derive(Debug)]
+pub struct PuzzleInput {
+    width: usize,
+    height: usize,
+    values: Vec<u32>,
+}
+
+impl PuzzleInput {
+    fn get(&self, x: usize, y: usize) -> u32 {
+        self.values[y * self.width + x]
+    }
+
+    fn get_neighbors(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
+        let deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+        let mut res = Vec::with_capacity(deltas.len());
+
+        for (dx, dy) in &deltas {
+            let rx = x as i64 + dx;
+            let ry = y as i64 + dy;
+
+            if 0 <= rx && rx < self.width as i64 && 0 <= ry && ry < self.height as i64 {
+                res.push((rx as usize, ry as usize))
+            }
+        }
+        res
+    }
+}
+
+impl ParseInput<Day9> for Aoc2021 {
+    type Parsed = PuzzleInput;
+
+    fn parse_input(input: &str) -> PuzzleInput {
+        let mut values = Vec::new();
+        let mut width = None;
+        let mut height = 0;
+        for line in input.lines() {
+            height += 1;
+
+            let line = line.trim();
+            if let Some(width) = width {
+                assert_eq!(width, line.len());
+            } else {
+                width = Some(line.len());
+            }
+
+            for c in line.chars() {
+                values.push(c.to_digit(10).unwrap())
+            }
+        }
+
+        PuzzleInput {
+            width: width.unwrap(),
+            height,
+            values,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct ColoredMap {
+    width: usize,
+    height: usize,
+    colors: Vec<u32>,
+    color_counter: u32,
+}
+
+impl ColoredMap {
+    fn get(&mut self, x: usize, y: usize) -> u32 {
+        self.colors[y * self.width + x]
+    }
+
+    fn set(&mut self, x: usize, y: usize, color: u32) {
+        self.colors[y * self.width + x] = color;
+    }
+
+    fn next_color(&mut self) -> u32 {
+        self.color_counter += 1;
+        self.color_counter
+    }
+}
+
+fn compute_lower_points(map: &PuzzleInput) -> Vec<(usize, usize)> {
+    let mut points = Vec::new();
+    for y in 0..map.height {
+        'main: for x in 0..map.width {
+            let current = map.get(x, y);
+
+            for (rx, ry) in map.get_neighbors(x, y) {
+                if map.get(rx as usize, ry as usize) <= current {
+                    continue 'main;
+                }
+            }
+            points.push((x, y))
+        }
+    }
+    points
+}
+
+impl Solution<Day9> for Aoc2021 {
+    type Part1Output = u32;
+    type Part2Output = usize;
+
+    fn part1(input: &PuzzleInput) -> u32 {
+        compute_lower_points(input)
+            .into_iter()
+            .map(|(x, y)| input.get(x, y) + 1)
+            .sum()
+    }
+
+    fn part2(input: &PuzzleInput) -> usize {
+        let mut colored_map = ColoredMap {
+            width: input.width,
+            height: input.height,
+            colors: vec![0; input.values.len()],
+            color_counter: 0,
+        };
+
+        for (x, y) in compute_lower_points(input) {
+            let color = colored_map.next_color();
+
+            let mut open_list = vec![(x, y)];
+
+            while let Some((x, y)) = open_list.pop() {
+                if colored_map.get(x, y) != 0 {
+                    continue;
+                }
+                if input.get(x, y) == 9 {
+                    continue;
+                }
+
+                colored_map.set(x, y, color);
+                open_list.extend(input.get_neighbors(x, y));
+            }
+        }
+
+        let mut color_count = vec![0; colored_map.color_counter as usize + 1];
+        for color in colored_map.colors {
+            if color != 0 {
+                color_count[color as usize] += 1;
+            }
+        }
+
+        color_count.sort();
+        color_count.into_iter().rev().take(3).product()
+    }
+}
--- src/aoc2021/mod.rs
+++ src/aoc2021/mod.rs
@@ -11,6 +11,7 @@ pub mod day5;
 pub mod day6;
 pub mod day7;
 pub mod day8;
+pub mod day9;
 
 pub fn run_solution_for_day(day: u32, input: &str, results: Option<Results>) {
     let r = results
@@ -26,6 +27,7 @@ pub fn run_solution_for_day(day: u32, input: &str, results: Option<Results>) {
         6 => run::<Aoc2021, Day6>(input, r),
         7 => run::<Aoc2021, Day7>(input, r),
         8 => run::<Aoc2021, Day8>(input, r),
+        9 => run::<Aoc2021, Day9>(input, r),
         _ => unimplemented!("no solution available for that day"),
     }
 }
