d231cc1f7a8753c793ce90dd52b3639968af3437
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-12-10 13:47:57 +0100

    Add script to generate new day

---
+++ scripts/day_template.rs
@@ -0,0 +1,25 @@
+use crate::aoc2019::Aoc2019;
+use advent_of_code_traits::days::Day1;
+use advent_of_code_traits::ParseInput;
+use advent_of_code_traits::Solution;
+
+impl ParseInput<Day1> for Aoc2019 {
+    type Parsed = _;
+
+    fn parse_input(input: &str) -> _ {
+        todo!()
+    }
+}
+
+impl Solution<Day1> for Aoc2019 {
+    type Part1Output = u32;
+    type Part2Output = u32;
+
+    fn part1(input: &_) -> u32 {
+        todo!()
+    }
+
+    fn part2(input: &_) -> u32 {
+        todo!()
+    }
+}
---
+++ scripts/new_day.py
@@ -0,0 +1,18 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Create new day file.')
+parser.add_argument('--year', type=int, help='year',
+                    default=2021, choices=[2019, 2021])
+parser.add_argument('--day', type=int, help='day', required=True)
+
+args = parser.parse_args()
+
+with open("./scripts/day_template.rs") as f:
+    template = f.read()
+
+new_content = template.replace(
+    "Day1", f"Day{args.day}").replace("2019", str(args.year))
+
+target_path = f"./src/aoc{args.year}/day{args.day}.rs"
+with open(target_path, "w") as f:
+    f.write(new_content)
