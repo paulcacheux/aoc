9838b645e8e5914d45447540a95b2c88aa1eb8be
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-12-12 14:46:26 +0100

    Day12 first version

---
+++ src/aoc2021/day12.rs
@@ -0,0 +1,125 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+use crate::aoc2021::Aoc2021;
+use advent_of_code_traits::days::Day12;
+use advent_of_code_traits::ParseInput;
+use advent_of_code_traits::Solution;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Node {
+    Start,
+    End,
+    Big(String),
+    Small(String),
+}
+
+impl ParseInput<Day12> for Aoc2021 {
+    type Parsed = Vec<(Node, Node)>;
+
+    fn parse_input(input: &str) -> Vec<(Node, Node)> {
+        fn part_parse(part: &str) -> Node {
+            match part {
+                "start" => Node::Start,
+                "end" => Node::End,
+                _ => {
+                    let first = part.chars().next().unwrap();
+                    if first.is_ascii_lowercase() {
+                        Node::Small(part.to_owned())
+                    } else {
+                        Node::Big(part.to_owned())
+                    }
+                }
+            }
+        }
+
+        input
+            .lines()
+            .map(|line| {
+                let mut parts = line.trim().split('-');
+                let left = parts.next().unwrap();
+                let right = parts.next().unwrap();
+                (part_parse(left), part_parse(right))
+            })
+            .collect()
+    }
+}
+
+fn build_links(input: &Vec<(Node, Node)>) -> HashMap<Node, HashSet<Node>> {
+    let mut links: HashMap<Node, HashSet<Node>> = HashMap::new();
+    for (a, b) in input {
+        links.entry(a.clone()).or_default().insert(b.clone());
+        links.entry(b.clone()).or_default().insert(a.clone());
+    }
+    links
+}
+
+fn count_paths(
+    links: &HashMap<Node, HashSet<Node>>,
+    visited_builder: fn(&[Node]) -> HashSet<Node>,
+) -> usize {
+    let mut working_set = vec![vec![Node::Start]];
+    let mut counter = 0;
+    while let Some(current) = working_set.pop() {
+        let last = &current[current.len() - 1];
+        if last == &Node::End {
+            counter += 1;
+            continue;
+        }
+
+        let visited_set = visited_builder(&current);
+
+        if let Some(linked) = links.get(last) {
+            for next in linked {
+                if !visited_set.contains(next) {
+                    let mut list = current.clone();
+                    list.push(next.clone());
+                    working_set.push(list);
+                }
+            }
+        }
+    }
+    counter
+}
+
+fn part1_visited_builder(current: &[Node]) -> HashSet<Node> {
+    current
+        .iter()
+        .filter(|n| match n {
+            Node::Big(_) => false,
+            _ => true,
+        })
+        .cloned()
+        .collect()
+}
+
+impl Solution<Day12> for Aoc2021 {
+    type Part1Output = usize;
+    type Part2Output = usize;
+
+    fn part1(input: &Vec<(Node, Node)>) -> usize {
+        let links = build_links(input);
+        count_paths(&links, part1_visited_builder)
+    }
+
+    fn part2(input: &Vec<(Node, Node)>) -> usize {
+        let links = build_links(input);
+        count_paths(&links, |current| {
+            let set = part1_visited_builder(current);
+            let mut counter: HashMap<&str, usize> = HashMap::new();
+            for node in current {
+                if let Node::Small(name) = node {
+                    *counter.entry(name).or_default() += 1;
+                }
+            }
+
+            if counter.values().any(|&c| c >= 2) {
+                set
+            } else {
+                set.into_iter()
+                    .filter(|n| if let Node::Small(_) = n { false } else { true })
+                    .collect()
+            }
+        })
+    }
+}
