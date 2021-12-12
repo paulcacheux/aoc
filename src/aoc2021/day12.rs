b8b1a025b70999ad13e20cdf7f98d6cd0fe57057
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-12-12 17:23:47 +0100

    Day12 improvements

--- src/aoc2021/day12.rs
+++ src/aoc2021/day12.rs
@@ -68,6 +68,75 @@ fn build_links(input: &[(Node, Node)]) -> HashMap<Node, HashSet<Node>> {
     links
 }
 
+trait State: Clone {
+    fn new() -> Self;
+    fn queue(&self) -> Node;
+    fn is_full(&self) -> bool;
+    fn can_go_to(&self, target: &Node) -> bool;
+    fn append(&mut self, node: Node);
+}
+
+#[derive(Debug, Clone)]
+struct Part1State {
+    queue: Node,
+    visited_set: HashSet<Node>,
+}
+
+impl State for Part1State {
+    fn new() -> Self {
+        let mut visited_set = HashSet::new();
+        visited_set.insert(Node::Start);
+        Part1State {
+            queue: Node::Start,
+            visited_set,
+        }
+    }
+
+    fn queue(&self) -> Node {
+        self.queue
+    }
+
+    fn is_full(&self) -> bool {
+        self.queue == Node::End
+    }
+
+    fn can_go_to(&self, target: &Node) -> bool {
+        !self.visited_set.contains(target)
+    }
+
+    fn append(&mut self, node: Node) {
+        match node {
+            Node::Start | Node::End | Node::Small(_) => {
+                self.visited_set.insert(node);
+            }
+            Node::Big(_) => {}
+        }
+        self.queue = node;
+    }
+}
+
+fn count_paths_v2<S: State>(links: &HashMap<Node, HashSet<Node>>) -> usize {
+    let mut working_set = vec![S::new()];
+    let mut counter = 0;
+    while let Some(current) = working_set.pop() {
+        if current.is_full() {
+            counter += 1;
+            continue;
+        }
+
+        if let Some(linked) = links.get(&current.queue()) {
+            for next in linked {
+                if current.can_go_to(next) {
+                    let mut state = current.clone();
+                    state.append(*next);
+                    working_set.push(state);
+                }
+            }
+        }
+    }
+    counter
+}
+
 fn count_paths(
     links: &HashMap<Node, HashSet<Node>>,
     visited_builder: fn(&[Node]) -> HashSet<Node>,
@@ -96,21 +165,13 @@ fn count_paths(
     counter
 }
 
-fn part1_visited_builder(current: &[Node]) -> HashSet<Node> {
-    current
-        .iter()
-        .filter(|n| !matches!(n, Node::Big(_)))
-        .cloned()
-        .collect()
-}
-
 impl Solution<Day12> for Aoc2021 {
     type Part1Output = usize;
     type Part2Output = usize;
 
     fn part1(input: &PuzzleInput) -> usize {
         let links = build_links(&input.pairs);
-        count_paths(&links, part1_visited_builder)
+        count_paths_v2::<Part1State>(&links)
     }
 
     fn part2(input: &PuzzleInput) -> usize {
