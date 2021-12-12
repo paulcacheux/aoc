35e53fa833ddb3829e6e233ef371cee7bba9db79
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-12-12 17:38:20 +0100

    Day12 code cleanup

--- src/aoc2021/day12.rs
+++ src/aoc2021/day12.rs
@@ -68,36 +68,48 @@ fn build_links(input: &[(Node, Node)]) -> HashMap<Node, HashSet<Node>> {
     links
 }
 
-trait State: Clone {
+#[derive(Debug, Clone)]
+struct PathState<VS: VisitedState> {
+    queue: Node,
+    visited_state: VS,
+}
+
+impl<VS: VisitedState> PathState<VS> {
+    fn new() -> Self {
+        let mut visited_set = HashSet::new();
+        visited_set.insert(Node::Start);
+        PathState {
+            queue: Node::Start,
+            visited_state: VS::new(),
+        }
+    }
+
+    fn is_full(&self) -> bool {
+        self.queue == Node::End
+    }
+
+    fn append(&mut self, node: Node) {
+        self.visited_state.append(node);
+        self.queue = node;
+    }
+}
+
+trait VisitedState: Clone {
     fn new() -> Self;
-    fn queue(&self) -> Node;
-    fn is_full(&self) -> bool;
     fn can_go_to(&self, target: &Node) -> bool;
     fn append(&mut self, node: Node);
 }
 
 #[derive(Debug, Clone)]
 struct Part1State {
-    queue: Node,
     visited_set: HashSet<Node>,
 }
 
-impl State for Part1State {
+impl VisitedState for Part1State {
     fn new() -> Self {
         let mut visited_set = HashSet::new();
         visited_set.insert(Node::Start);
-        Part1State {
-            queue: Node::Start,
-            visited_set,
-        }
-    }
-
-    fn queue(&self) -> Node {
-        self.queue
-    }
-
-    fn is_full(&self) -> bool {
-        self.queue == Node::End
+        Part1State { visited_set }
     }
 
     fn can_go_to(&self, target: &Node) -> bool {
@@ -111,36 +123,25 @@ impl State for Part1State {
             }
             Node::Big(_) => {}
         }
-        self.queue = node;
     }
 }
 
 #[derive(Debug, Clone)]
 struct Part2State {
-    queue: Node,
     visited_set: HashSet<Node>,
     smalls_visited: HashSet<DefaultSymbol>,
     double_checked: bool,
 }
 
-impl State for Part2State {
+impl VisitedState for Part2State {
     fn new() -> Self {
         let mut visited_set = HashSet::new();
         visited_set.insert(Node::Start);
         Part2State {
-            queue: Node::Start,
             visited_set,
             smalls_visited: HashSet::default(),
             double_checked: false,
         }
-    }
-
-    fn queue(&self) -> Node {
-        self.queue
-    }
-
-    fn is_full(&self) -> bool {
-        self.queue == Node::End
     }
 
     fn can_go_to(&self, target: &Node) -> bool {
@@ -165,12 +166,11 @@ impl State for Part2State {
             }
             Node::Big(_) => {}
         }
-        self.queue = node;
     }
 }
 
-fn count_paths_v2<S: State>(links: &HashMap<Node, HashSet<Node>>) -> usize {
-    let mut working_set = vec![S::new()];
+fn count_paths_v2<VS: VisitedState>(links: &HashMap<Node, HashSet<Node>>) -> usize {
+    let mut working_set = vec![PathState::<VS>::new()];
     let mut counter = 0;
     while let Some(current) = working_set.pop() {
         if current.is_full() {
@@ -178,9 +178,9 @@ fn count_paths_v2<S: State>(links: &HashMap<Node, HashSet<Node>>) -> usize {
             continue;
         }
 
-        if let Some(linked) = links.get(&current.queue()) {
+        if let Some(linked) = links.get(&current.queue) {
             for next in linked {
-                if current.can_go_to(next) {
+                if current.visited_state.can_go_to(next) {
                     let mut state = current.clone();
                     state.append(*next);
                     working_set.push(state);
