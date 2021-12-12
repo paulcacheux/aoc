28c42be1e15fda0c2f0ed5c660a713e7ff712129
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-12-12 17:30:54 +0100

    Day12 improvements

--- src/aoc2021/day12.rs
+++ src/aoc2021/day12.rs
@@ -115,6 +115,60 @@ impl State for Part1State {
     }
 }
 
+#[derive(Debug, Clone)]
+struct Part2State {
+    queue: Node,
+    visited_set: HashSet<Node>,
+    smalls_visited: HashSet<DefaultSymbol>,
+    double_checked: bool,
+}
+
+impl State for Part2State {
+    fn new() -> Self {
+        let mut visited_set = HashSet::new();
+        visited_set.insert(Node::Start);
+        Part2State {
+            queue: Node::Start,
+            visited_set,
+            smalls_visited: HashSet::default(),
+            double_checked: false,
+        }
+    }
+
+    fn queue(&self) -> Node {
+        self.queue
+    }
+
+    fn is_full(&self) -> bool {
+        self.queue == Node::End
+    }
+
+    fn can_go_to(&self, target: &Node) -> bool {
+        !self.visited_set.contains(target)
+    }
+
+    fn append(&mut self, node: Node) {
+        match node {
+            Node::Start | Node::End => {
+                self.visited_set.insert(node);
+            }
+            Node::Small(name) => {
+                if self.double_checked {
+                    self.visited_set.insert(node);
+                } else if self.smalls_visited.contains(&name) {
+                    self.visited_set
+                        .extend(self.smalls_visited.drain().map(Node::Small));
+                    self.double_checked = true;
+                } else {
+                    self.smalls_visited.insert(name);
+                }
+            }
+            Node::Big(_) => {}
+        }
+        self.queue = node;
+    }
+}
+
 fn count_paths_v2<S: State>(links: &HashMap<Node, HashSet<Node>>) -> usize {
     let mut working_set = vec![S::new()];
     let mut counter = 0;
@@ -137,34 +191,6 @@ fn count_paths_v2<S: State>(links: &HashMap<Node, HashSet<Node>>) -> usize {
     counter
 }
 
-fn count_paths(
-    links: &HashMap<Node, HashSet<Node>>,
-    visited_builder: fn(&[Node]) -> HashSet<Node>,
-) -> usize {
-    let mut working_set = vec![vec![Node::Start]];
-    let mut counter = 0;
-    while let Some(current) = working_set.pop() {
-        let last = &current[current.len() - 1];
-        if last == &Node::End {
-            counter += 1;
-            continue;
-        }
-
-        let visited_set = visited_builder(&current);
-
-        if let Some(linked) = links.get(last) {
-            for next in linked {
-                if !visited_set.contains(next) {
-                    let mut list = current.clone();
-                    list.push(*next);
-                    working_set.push(list);
-                }
-            }
-        }
-    }
-    counter
-}
-
 impl Solution<Day12> for Aoc2021 {
     type Part1Output = usize;
     type Part2Output = usize;
@@ -176,26 +202,6 @@ impl Solution<Day12> for Aoc2021 {
 
     fn part2(input: &PuzzleInput) -> usize {
         let links = build_links(&input.pairs);
-        count_paths(&links, |current| {
-            let mut set = HashSet::new();
-            let mut counter: HashMap<DefaultSymbol, usize> = HashMap::new();
-            for node in current {
-                match node {
-                    Node::Start | Node::End => {
-                        set.insert(*node);
-                    }
-                    Node::Big(_) => {}
-                    Node::Small(name) => {
-                        set.insert(*node);
-                        *counter.entry(*name).or_default() += 1;
-                    }
-                }
-            }
-
-            if !counter.values().any(|&c| c >= 2) {
-                set.retain(|n| !matches!(n, Node::Small(_)))
-            }
-            set
-        })
+        count_paths_v2::<Part2State>(&links)
     }
 }
