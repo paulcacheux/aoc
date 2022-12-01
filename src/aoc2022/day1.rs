00500ac23d4df747eade36096c526348eac815fa
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-12-01 09:43:08 +0100

    Fix clippy

--- src/aoc2022/day1.rs
+++ src/aoc2022/day1.rs
@@ -28,13 +28,13 @@ impl Solution<Day1> for Aoc2022 {
     type Part2Output = u32;
 
     fn part1(input: &Vec<Vec<u32>>) -> u32 {
-        input.into_iter().map(|elf| {
+        input.iter().map(|elf| {
             elf.iter().sum()
         }).max().unwrap()
     }
 
     fn part2(input: &Vec<Vec<u32>>) -> u32 {
-        let mut sums: Vec<u32> = input.into_iter().map(|elf| {
+        let mut sums: Vec<u32> = input.iter().map(|elf| {
             elf.iter().sum()
         }).collect();
 
