cc3f51459ae47e26756d804d5832cfa104b0f81f
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-12-03 13:53:42 +0100

    Day 3 optimized

--- src/aoc2022/day3.rs
+++ src/aoc2022/day3.rs
@@ -3,7 +3,6 @@ use advent_of_code_traits::days::Day3;
 use advent_of_code_traits::ParseInput;
 use advent_of_code_traits::Solution;
 use itertools::Itertools;
-use std::collections::HashSet;
 
 impl ParseInput<Day3> for Aoc2022 {
     type Parsed = Vec<Vec<u8>>;
@@ -24,38 +23,57 @@ fn priority(c: u8) -> u8 {
     }
 }
 
+fn intersection_p1(left: &[u8], right: &[u8]) -> Option<u8> {
+    for &y in right {
+        for &x in left {
+            if x == y {
+                return Some(x);
+            }
+        }
+    }
+    None
+}
+
+fn intersection_p2(left: &[u8], right: &[u8]) -> Vec<u8> {
+    let mut res = Vec::with_capacity(left.len());
+    for &y in right {
+        for &x in left {
+            if x == y {
+                res.push(x);
+            }
+        }
+    }
+    res.sort();
+    res.dedup();
+    res
+}
+
 impl Solution<Day3> for Aoc2022 {
     type Part1Output = u32;
     type Part2Output = u32;
 
     fn part1(input: &Vec<Vec<u8>>) -> u32 {
-        let mut res = 0;
-        for line in input {
-            assert_eq!(line.len() % 2, 0);
-
-            let (left, right) = line.split_at(line.len() / 2);
-            let left: HashSet<_> = left.iter().copied().collect();
-            let right: HashSet<_> = right.iter().copied().collect();
-            res += left
-                .intersection(&right)
-                .map(|&c| priority(c) as u32)
-                .sum::<u32>();
-        }
-        res
+        input
+            .iter()
+            .map(|line| {
+                let (left, right) = line.split_at(line.len() / 2);
+                priority(intersection_p1(left, right).unwrap_or_default()) as u32
+            })
+            .sum()
     }
 
     fn part2(input: &Vec<Vec<u8>>) -> u32 {
-        let mut res = 0;
-        for chunks in input.iter().chunks(3).into_iter() {
-            let common = chunks
-                .map(|line| line.iter().copied().collect::<HashSet<_>>())
-                .reduce(|accum, item| accum.intersection(&item).copied().collect())
-                .unwrap_or_default();
-            assert_eq!(common.len(), 1);
-
-            let common = common.into_iter().next().unwrap();
-            res += priority(common) as u32;
-        }
-        res
+        input
+            .iter()
+            .chunks(3)
+            .into_iter()
+            .map(|chunks| {
+                let common = chunks
+                    .cloned()
+                    .reduce(|accum, item| intersection_p2(&accum, &item))
+                    .unwrap_or_default();
+                priority(common[0]) as u32
+            })
+            .sum()
     }
 }
