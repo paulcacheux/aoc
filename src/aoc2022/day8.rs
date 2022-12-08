e11e7bb8a40814e6b3844843bebb5f3212ed7d40
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-12-08 12:06:54 +0100

    Small cleanup of iteration

--- src/aoc2022/day8.rs
+++ src/aoc2022/day8.rs
@@ -14,6 +14,13 @@ impl Grid {
     fn get(&self, x: usize, y: usize) -> u8 {
         self.heights[self.width * y + x]
     }
+
+    fn iter<'a>(&'a self) -> impl Iterator<Item = (usize, usize, u8)> + 'a {
+        self.heights
+            .iter()
+            .enumerate()
+            .map(|(i, &val)| (i % self.width, i / self.width, val))
+    }
 }
 
 impl ParseInput<Day8> for Aoc2022 {
@@ -49,30 +56,26 @@ impl Solution<Day8> for Aoc2022 {
 
     fn part1(input: &Grid) -> u32 {
         let mut counter = 0;
-        for y in 0..input.height {
-            for x in 0..input.width {
-                let current_tree = input.get(x, y);
-
-                // left
-                if (0..x).all(|dx| input.get(dx, y) < current_tree) {
-                    counter += 1;
-                    continue;
-                }
-                // right
-                if ((x + 1)..input.width).all(|dx| input.get(dx, y) < current_tree) {
-                    counter += 1;
-                    continue;
-                }
-                // up
-                if (0..y).all(|dy| input.get(x, dy) < current_tree) {
-                    counter += 1;
-                    continue;
-                }
-                // down
-                if ((y + 1)..input.height).all(|dy| input.get(x, dy) < current_tree) {
-                    counter += 1;
-                    continue;
-                }
+        for (x, y, current_tree) in input.iter() {
+            // left
+            if (0..x).all(|dx| input.get(dx, y) < current_tree) {
+                counter += 1;
+                continue;
+            }
+            // right
+            if ((x + 1)..input.width).all(|dx| input.get(dx, y) < current_tree) {
+                counter += 1;
+                continue;
+            }
+            // up
+            if (0..y).all(|dy| input.get(x, dy) < current_tree) {
+                counter += 1;
+                continue;
+            }
+            // down
+            if ((y + 1)..input.height).all(|dy| input.get(x, dy) < current_tree) {
+                counter += 1;
+                continue;
             }
         }
         counter
@@ -80,32 +83,28 @@ impl Solution<Day8> for Aoc2022 {
 
     fn part2(input: &Grid) -> usize {
         let mut best_score = 0;
-        for y in 0..input.height {
-            for x in 0..input.width {
-                if x == 0 || y == 0 || x == input.width - 1 || y == input.height - 1 {
-                    continue;
-                }
+        for (x, y, current_tree) in input.iter() {
+            if x == 0 || y == 0 || x == input.width - 1 || y == input.height - 1 {
+                continue;
+            }
 
-                let current_tree = input.get(x, y);
+            let mut score = 1;
+            // left
+            score *= part2_search(0..x, true, |dx| input.get(dx, y) < current_tree);
+            // right
+            score *= part2_search((x + 1)..input.width, false, |dx| {
+                input.get(dx, y) < current_tree
+            });
 
-                let mut score = 1;
-                // left
-                score *= part2_search(0..x, true, |dx| input.get(dx, y) < current_tree);
-                // right
-                score *= part2_search((x + 1)..input.width, false, |dx| {
-                    input.get(dx, y) < current_tree
-                });
+            // up
+            score *= part2_search(0..y, true, |dy| input.get(x, dy) < current_tree);
+            // down
+            score *= part2_search((y + 1)..input.height, false, |dy| {
+                input.get(x, dy) < current_tree
+            });
 
-                // up
-                score *= part2_search(0..y, true, |dy| input.get(x, dy) < current_tree);
-                // down
-                score *= part2_search((y + 1)..input.height, false, |dy| {
-                    input.get(x, dy) < current_tree
-                });
-
-                if score > best_score {
-                    best_score = score;
-                }
+            if score > best_score {
+                best_score = score;
             }
         }
         best_score
