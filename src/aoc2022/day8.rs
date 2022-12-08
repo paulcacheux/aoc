5b8dfb83220b03a76d9e5e7e72f9b76a8a27bb8c
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-12-08 12:13:52 +0100

    Functional programming FTW

--- src/aoc2022/day8.rs
+++ src/aoc2022/day8.rs
@@ -51,63 +51,48 @@ impl ParseInput<Day8> for Aoc2022 {
 }
 
 impl Solution<Day8> for Aoc2022 {
-    type Part1Output = u32;
+    type Part1Output = usize;
     type Part2Output = usize;
 
-    fn part1(input: &Grid) -> u32 {
-        let mut counter = 0;
-        for (x, y, current_tree) in input.iter() {
-            // left
-            if (0..x).all(|dx| input.get(dx, y) < current_tree) {
-                counter += 1;
-                continue;
-            }
-            // right
-            if ((x + 1)..input.width).all(|dx| input.get(dx, y) < current_tree) {
-                counter += 1;
-                continue;
-            }
-            // up
-            if (0..y).all(|dy| input.get(x, dy) < current_tree) {
-                counter += 1;
-                continue;
-            }
-            // down
-            if ((y + 1)..input.height).all(|dy| input.get(x, dy) < current_tree) {
-                counter += 1;
-                continue;
-            }
-        }
-        counter
+    fn part1(input: &Grid) -> usize {
+        input
+            .iter()
+            .filter(|&(x, y, val)| {
+                (0..x).all(|dx| input.get(dx, y) < val) // left
+                    || ((x + 1)..input.width).all(|dx| input.get(dx, y) < val) // right
+                    || (0..y).all(|dy| input.get(x, dy) < val) // up
+                    || ((y + 1)..input.height).all(|dy| input.get(x, dy) < val) // down
+            })
+            .count()
     }
 
     fn part2(input: &Grid) -> usize {
-        let mut best_score = 0;
-        for (x, y, current_tree) in input.iter() {
-            if x == 0 || y == 0 || x == input.width - 1 || y == input.height - 1 {
-                continue;
-            }
+        input
+            .iter()
+            .filter_map(|(x, y, current_tree)| {
+                if x == 0 || y == 0 || x == input.width - 1 || y == input.height - 1 {
+                    return None;
+                }
 
-            let mut score = 1;
-            // left
-            score *= part2_search(0..x, true, |dx| input.get(dx, y) < current_tree);
-            // right
-            score *= part2_search((x + 1)..input.width, false, |dx| {
-                input.get(dx, y) < current_tree
-            });
+                let mut score = 1;
+                // left
+                score *= part2_search(0..x, true, |dx| input.get(dx, y) < current_tree);
+                // right
+                score *= part2_search((x + 1)..input.width, false, |dx| {
+                    input.get(dx, y) < current_tree
+                });
 
-            // up
-            score *= part2_search(0..y, true, |dy| input.get(x, dy) < current_tree);
-            // down
-            score *= part2_search((y + 1)..input.height, false, |dy| {
-                input.get(x, dy) < current_tree
-            });
+                // up
+                score *= part2_search(0..y, true, |dy| input.get(x, dy) < current_tree);
+                // down
+                score *= part2_search((y + 1)..input.height, false, |dy| {
+                    input.get(x, dy) < current_tree
+                });
 
-            if score > best_score {
-                best_score = score;
-            }
-        }
-        best_score
+                Some(score)
+            })
+            .max()
+            .unwrap()
     }
 }
 
