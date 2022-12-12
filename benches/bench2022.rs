00efe520ff773b3207f37612d2e3c55af76f8b52
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-12-12 21:11:36 +0100

    actual benchmark

--- benches/bench2022.rs
+++ benches/bench2022.rs
@@ -1,15 +1,17 @@
+use std::hint::black_box;
+
 use aoc::traits::{days::Day12, ParseInput, Solution};
 use criterion::{criterion_group, criterion_main, Criterion};
 
-fn run_day12() {
-    let input = std::fs::read_to_string("./inputs/2022/day12.txt").unwrap();
+fn run_day12(input: &str) {
     let input = <aoc::aoc2022::Aoc2022 as ParseInput<Day12>>::parse_input(&input);
-    let _part1res = <aoc::aoc2022::Aoc2022 as Solution<Day12>>::part1(&input);
-    let _part2res = <aoc::aoc2022::Aoc2022 as Solution<Day12>>::part2(&input);
+    black_box(<aoc::aoc2022::Aoc2022 as Solution<Day12>>::part1(&input));
+    black_box(<aoc::aoc2022::Aoc2022 as Solution<Day12>>::part2(&input));
 }
 
 fn bench_day12(c: &mut Criterion) {
-    c.bench_function("day 12", |b| b.iter(|| run_day12()));
+    let input = std::fs::read_to_string("./inputs/2022/day12.txt").unwrap();
+    c.bench_function("day 12", |b| b.iter(|| run_day12(&input)));
 }
 
 criterion_group!(benches, bench_day12);
