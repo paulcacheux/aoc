b4fe090acf96c47bddaa52e4c44f5ab284aee717
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-12-12 21:34:16 +0100

    add more benchmarks

--- benches/bench2022.rs
+++ benches/bench2022.rs
@@ -1,18 +1,54 @@
+#![feature(concat_idents)]
+
 use std::hint::black_box;
 
-use aoc::traits::{days::Day12, ParseInput, Solution};
+use aoc::traits::{days::*, ParseInput, Solution};
 use criterion::{criterion_group, criterion_main, Criterion};
 
-fn run_day12(input: &str) {
-    let input = <aoc::aoc2022::Aoc2022 as ParseInput<Day12>>::parse_input(&input);
-    black_box(<aoc::aoc2022::Aoc2022 as Solution<Day12>>::part1(&input));
-    black_box(<aoc::aoc2022::Aoc2022 as Solution<Day12>>::part2(&input));
+macro_rules! gen_bench {
+    ($benchday:ident, $dayconst:ident) => {
+        fn $benchday(c: &mut Criterion) {
+            fn runner(input: &str) {
+                let input = <aoc::aoc2022::Aoc2022 as ParseInput<$dayconst>>::parse_input(&input);
+                black_box(<aoc::aoc2022::Aoc2022 as Solution<$dayconst>>::part1(
+                    &input,
+                ));
+                black_box(<aoc::aoc2022::Aoc2022 as Solution<$dayconst>>::part2(
+                    &input,
+                ));
+            }
+
+            let input = std::fs::read_to_string(format!(
+                "./inputs/2022/{}.txt",
+                stringify!($dayconst).to_lowercase()
+            ))
+            .unwrap();
+            c.bench_function(stringify!($dayconst), |b| b.iter(|| runner(&input)));
+        }
+    };
 }
 
-fn bench_day12(c: &mut Criterion) {
-    let input = std::fs::read_to_string("./inputs/2022/day12.txt").unwrap();
-    c.bench_function("day 12", |b| b.iter(|| run_day12(&input)));
+macro_rules! gen_bench_group {
+    ($(($benchday:ident, $dayconst:ident)),+) => {
+        $(gen_bench!($benchday, $dayconst);)+
+
+        criterion_group!(benches, $($benchday,)+);
+    };
 }
 
-criterion_group!(benches, bench_day12);
+gen_bench_group!(
+    (bench_day1, Day1),
+    (bench_day2, Day2),
+    (bench_day3, Day3),
+    (bench_day4, Day4),
+    (bench_day5, Day5),
+    (bench_day6, Day6),
+    (bench_day7, Day7),
+    (bench_day8, Day8),
+    (bench_day9, Day9),
+    (bench_day10, Day10),
+    (bench_day11, Day11),
+    (bench_day12, Day12)
+);
+
 criterion_main!(benches);
