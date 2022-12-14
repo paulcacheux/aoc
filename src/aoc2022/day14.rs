9fc56e0e66bb511fa82f1ea880f5325f1d393d18
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-12-14 12:37:11 +0100

    part 2

--- src/aoc2022/day14.rs
+++ src/aoc2022/day14.rs
@@ -98,8 +98,76 @@ impl Solution<Day14> for Aoc2022 {
         counter
     }
 
-    fn part2(_input: &Vec<Vec<(u32, u32)>>) -> u32 {
-        todo!()
+    fn part2(input: &Vec<Vec<(u32, u32)>>) -> u32 {
+        let mut minx = u32::MAX;
+        let mut maxx = 0;
+        let mut miny = u32::MAX;
+        let mut maxy = 0;
+        for points in input.iter().chain([&vec![SAND_FOUNTAIN]]) {
+            for &(x, y) in points {
+                if x < minx {
+                    minx = x;
+                }
+                if x > maxx {
+                    maxx = x;
+                }
+                if y < miny {
+                    miny = y;
+                }
+                if y > maxy {
+                    maxy = y;
+                }
+            }
+        }
+
+        // part 2 required 1 more layer
+        // and a bit of space around
+        maxy += 1;
+        minx -= maxy;
+        maxx += maxy;
+
+        let mut grid = Grid::new(
+            (maxx - minx + 1) as usize,
+            (maxy - miny + 1) as usize,
+            Cell::Air,
+        );
+
+        for walls in input {
+            for [(sx, sy), (ex, ey)] in walls.array_windows::<2>() {
+                for x in range(*sx, *ex) {
+                    for y in range(*sy, *ey) {
+                        let x = x - minx;
+                        let y = y - miny;
+                        grid.set(x as usize, y as usize, Cell::Rock);
+                    }
+                }
+            }
+        }
+
+        let mut counter = 1;
+        grid.set(
+            (SAND_FOUNTAIN.0 - minx) as usize,
+            (SAND_FOUNTAIN.1 - miny) as usize,
+            Cell::Sand,
+        );
+        for y in 1..grid.height {
+            let y = y as usize;
+            for x in 0..grid.width {
+                if *grid.get(x, y) != Cell::Air {
+                    continue;
+                }
+
+                if [-1, 0, 1]
+                    .into_iter()
+                    .filter_map(|dx| offset_and_validate(grid.width, x as u32, dx))
+                    .any(|nx| *grid.get(nx as usize, y - 1) == Cell::Sand)
+                {
+                    grid.set(x, y, Cell::Sand);
+                    counter += 1;
+                }
+            }
+        }
+        counter
     }
 }
 
