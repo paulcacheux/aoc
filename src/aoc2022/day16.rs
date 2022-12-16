7d41e70d8f3e097bf529b730794b42947e66d409
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-12-16 22:27:32 +0100

    day16 part1

--- src/aoc2022/day16.rs
+++ src/aoc2022/day16.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use crate::aoc2022::Aoc2022;
 use crate::traits::days::Day16;
 use crate::traits::ParseInput;
@@ -45,25 +47,107 @@ impl Solution<Day16> for Aoc2022 {
     type Part1Output = u32;
     type Part2Output = u32;
 
-    fn part1(input: &Vec<Valve>) -> u32 {}
+    fn part1(input: &Vec<Valve>) -> u32 {
+        let interesting_valve_names: Vec<_> = input
+            .iter()
+            .filter_map(|valve| {
+                if valve.rate != 0 || valve.name == "AA" {
+                    Some(valve.name.clone())
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        let valves: HashMap<_, _> = input
+            .iter()
+            .map(|valve| (valve.name.clone(), valve.clone()))
+            .collect();
+
+        // bfs from all interesting points (and "AA") to other interesting points
+        let mut costs = HashMap::default();
+        for name in interesting_valve_names {
+            costs.insert(
+                name.clone(),
+                bfs(&valves, name)
+                    .into_iter()
+                    .filter(|(name, _)| valves[name].rate != 0)
+                    .collect::<Vec<_>>(),
+            );
+        }
+
+        // dfs
+        let mut queue = vec![Path {
+            nodes: vec!["AA".to_owned()],
+            last: "AA".to_owned(),
+            total_rate: 0,
+            time: 0,
+        }];
+
+        let mut res = Vec::new();
+
+        while let Some(current) = queue.pop() {
+            let mut found_next = false;
+            for (next, cost) in &costs[(&current.last)] {
+                if current.nodes.contains(next) {
+                    continue;
+                }
+
+                let time = current.time + *cost + 1;
+                if time > 30 {
+                    continue;
+                }
+
+                found_next = true;
+                let mut nodes = current.nodes.clone();
+                nodes.push(next.clone());
+
+                queue.push(Path {
+                    nodes,
+                    last: next.clone(),
+                    total_rate: current.total_rate + valves[next].rate * (30 - time),
+                    time,
+                })
+            }
+
+            if !found_next {
+                res.push(current);
+            }
+        }
+
+        res.into_iter().map(|path| path.total_rate).max().unwrap()
+    }
 
     fn part2(_input: &Vec<Valve>) -> u32 {
         todo!()
     }
 }
 
-#[derive(Debug)]
-struct Path {
-    nodes: Vec<(String, bool)>,
-    score: u32,
+fn bfs(edges: &HashMap<String, Valve>, start: String) -> HashMap<String, u32> {
+    let mut open_queue = VecDeque::new();
+    open_queue.push_back((start.clone(), 0));
+
+    let mut visited = HashMap::default();
+
+    while let Some((current, cost)) = open_queue.pop_front() {
+        if current != start {
+            visited.insert(current.clone(), cost);
+        }
+
+        let current = edges.get(&current).unwrap();
+        for next_name in &current.edges {
+            if !visited.contains_key(next_name) {
+                open_queue.push_back((next_name.clone(), cost + 1));
+            }
+        }
+    }
+    visited
 }
 
-impl Path {
-    fn last(&self) -> &str {
-        &self.nodes.last().unwrap().0
-    }
-
-    fn time(&self) -> u32 {
-        self.nodes.len() as u32
-    }
+#[derive(Debug)]
+struct Path {
+    nodes: Vec<String>,
+    last: String,
+    total_rate: u32,
+    time: u32,
 }
