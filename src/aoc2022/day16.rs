fa9fba000fad1984a289bfdf11f5ea16ef1d590d
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-12-16 23:02:57 +0100

    part 2

--- src/aoc2022/day16.rs
+++ src/aoc2022/day16.rs
@@ -6,6 +6,7 @@ use crate::traits::ParseInput;
 use crate::traits::Solution;
 
 use ahash::HashMap;
+use ahash::HashSet;
 use regex::Regex;
 
 #[derive(Debug, Clone)]
@@ -48,78 +49,26 @@ impl Solution<Day16> for Aoc2022 {
     type Part2Output = u32;
 
     fn part1(input: &Vec<Valve>) -> u32 {
-        let interesting_valve_names: Vec<_> = input
-            .iter()
-            .filter_map(|valve| {
-                if valve.rate != 0 || valve.name == "AA" {
-                    Some(valve.name.clone())
-                } else {
-                    None
-                }
-            })
-            .collect();
-
-        let valves: HashMap<_, _> = input
-            .iter()
-            .map(|valve| (valve.name.clone(), valve.clone()))
-            .collect();
-
-        // bfs from all interesting points (and "AA") to other interesting points
-        let mut costs = HashMap::default();
-        for name in interesting_valve_names {
-            costs.insert(
-                name.clone(),
-                bfs(&valves, name)
-                    .into_iter()
-                    .filter(|(name, _)| valves[name].rate != 0)
-                    .collect::<Vec<_>>(),
-            );
-        }
-
-        // dfs
-        let mut queue = vec![Path {
-            nodes: vec!["AA".to_owned()],
-            last: "AA".to_owned(),
-            total_rate: 0,
-            time: 0,
-        }];
-
-        let mut res = Vec::new();
-
-        while let Some(current) = queue.pop() {
-            let mut found_next = false;
-            for (next, cost) in &costs[(&current.last)] {
-                if current.nodes.contains(next) {
-                    continue;
-                }
-
-                let time = current.time + *cost + 1;
-                if time > 30 {
-                    continue;
-                }
-
-                found_next = true;
-                let mut nodes = current.nodes.clone();
-                nodes.push(next.clone());
-
-                queue.push(Path {
-                    nodes,
-                    last: next.clone(),
-                    total_rate: current.total_rate + valves[next].rate * (30 - time),
-                    time,
-                })
-            }
-
-            if !found_next {
-                res.push(current);
-            }
-        }
-
-        res.into_iter().map(|path| path.total_rate).max().unwrap()
+        let paths = solve_part1(input, 30);
+        paths.into_iter().map(|path| path.total_rate).max().unwrap()
     }
 
-    fn part2(_input: &Vec<Valve>) -> u32 {
-        todo!()
+    fn part2(input: &Vec<Valve>) -> u32 {
+        let paths = solve_part1(input, 26);
+        let mut max = 0;
+        for a in &paths {
+            for b in &paths {
+                if !a.nodes.is_disjoint(&b.nodes) {
+                    continue;
+                }
+
+                let rate = a.total_rate + b.total_rate;
+                if rate > max {
+                    max = rate;
+                }
+            }
+        }
+        max
     }
 }
 
@@ -146,8 +95,80 @@ fn bfs(edges: &HashMap<String, Valve>, start: String) -> HashMap<String, u32> {
 
 #[derive(Debug)]
 struct Path {
-    nodes: Vec<String>,
+    nodes: HashSet<String>,
     last: String,
     total_rate: u32,
     time: u32,
+}
+
+fn solve_part1(input: &[Valve], steps: u32) -> Vec<Path> {
+    let interesting_valve_names: Vec<_> = input
+        .iter()
+        .filter_map(|valve| {
+            if valve.rate != 0 || valve.name == "AA" {
+                Some(valve.name.clone())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let valves: HashMap<_, _> = input
+        .iter()
+        .map(|valve| (valve.name.clone(), valve.clone()))
+        .collect();
+
+    // bfs from all interesting points (and "AA") to other interesting points
+    let mut costs = HashMap::default();
+    for name in interesting_valve_names {
+        costs.insert(
+            name.clone(),
+            bfs(&valves, name)
+                .into_iter()
+                .filter(|(name, _)| valves[name].rate != 0)
+                .collect::<Vec<_>>(),
+        );
+    }
+
+    // dfs
+    let mut queue = vec![Path {
+        nodes: HashSet::default(),
+        last: "AA".to_owned(),
+        total_rate: 0,
+        time: 0,
+    }];
+
+    let mut paths = Vec::new();
+
+    while let Some(current) = queue.pop() {
+        let mut found_next = false;
+        for (next, cost) in &costs[(&current.last)] {
+            if current.nodes.contains(next) {
+                continue;
+            }
+
+            let time = current.time + *cost + 1;
+            if time > steps {
+                continue;
+            }
+
+            found_next = true;
+            let mut nodes = current.nodes.clone();
+            nodes.insert(next.clone());
+
+            queue.push(Path {
+                nodes,
+                last: next.clone(),
+                total_rate: current.total_rate + valves[next].rate * (steps - time),
+                time,
+            })
+        }
+
+        // for test case, since we have the time to switch on all valves
+        // we need to not check this boolean
+        if !found_next {
+            paths.push(current);
+        }
+    }
+    paths
 }
