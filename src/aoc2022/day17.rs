1d43329e072c6e6872d45920c66f0172fb817a8b
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-12-17 14:40:30 +0100

    part2 poc

--- src/aoc2022/day17.rs
+++ src/aoc2022/day17.rs
@@ -21,94 +21,60 @@ impl ParseInput<Day17> for Aoc2022 {
 
 impl Solution<Day17> for Aoc2022 {
     type Part1Output = usize;
-    type Part2Output = u32;
+    type Part2Output = usize;
 
     fn part1(input: &Vec<i32>) -> usize {
-        let blocks = vec![
-            vec![
-                [true, false, false, false],
-                [true, false, false, false],
-                [true, false, false, false],
-                [true, false, false, false],
-            ], // horizontal line
-            vec![
-                [false, true, false, false],
-                [true, true, true, false],
-                [false, true, false, false],
-            ], // cross
-            vec![
-                [true, false, false, false],
-                [true, false, false, false],
-                [true, true, true, false],
-            ], // reverse L
-            vec![[true, true, true, true]], // vertical line
-            vec![[true, true, false, false], [true, true, false, false]], // square
-        ];
-
-        const STEPS: usize = 2022;
-        let mut wind = Repeater::new(input);
-        let mut current_state = vec![Vec::with_capacity(STEPS * 4); 7];
-
-        for b in 0..STEPS {
-            let current_block = &blocks[b % blocks.len()];
-            let mut height = current_state.iter().map(Vec::len).max().unwrap() + 3;
-            let mut xoff = 2;
-
-            let mut step_state = true;
-
-            loop {
-                let new_xoff;
-                let new_height;
-                let mut bottom = false;
-                if step_state {
-                    new_xoff = (xoff + wind.next()).clamp(0, 7 - current_block.len() as i32);
-                    new_height = height;
-                } else {
-                    if height > 0 {
-                        new_height = height - 1;
-                    } else {
-                        new_height = height;
-                        bottom = true;
-                    }
-                    new_xoff = xoff;
-                }
-
-                let mut valid = true; // allow detection of bottom
-                'main: for i in 0..current_block.len() {
-                    for y in 0..4 {
-                        if current_block[i][y]
-                            && get(&current_state[new_xoff as usize + i], new_height + y)
-                        {
-                            valid = false;
-                            break 'main;
-                        }
-                    }
-                }
-
-                if (!valid || bottom) && !step_state {
-                    for i in 0..current_block.len() {
-                        for y in 0..4 {
-                            if current_block[i][y] {
-                                set(&mut current_state[xoff as usize + i], height + y);
-                            }
-                        }
-                    }
-                    break;
-                }
-                if valid {
-                    height = new_height;
-                    xoff = new_xoff;
-                }
-                step_state = !step_state;
-            }
-            // dbg_state(&current_state);
-        }
-
-        current_state.iter().map(Vec::len).max().unwrap()
+        solve_part1(input, 2022, None)
     }
 
-    fn part2(_input: &Vec<i32>) -> u32 {
-        todo!()
+    fn part2(input: &Vec<i32>) -> usize {
+        let base_period = input.len() * 5; // wind period * block period
+        let mut heights = Vec::new();
+        solve_part1(input, base_period * 4096, Some(&mut heights));
+
+        dbg!("found heights");
+
+        let mut per_heights = heights.clone();
+        const STABLE: usize = 20;
+
+        let mut best_factor = None;
+        for factor in (1..128).rev() {
+            let mut current_bottom = 0;
+            for (i, h) in per_heights.iter_mut().enumerate() {
+                if i % (base_period * factor) == 0 {
+                    current_bottom = *h;
+                }
+                *h -= current_bottom;
+            }
+
+            // skip the beginning
+            if per_heights[base_period * factor * STABLE..base_period * factor * (STABLE + 1)]
+                == per_heights
+                    [base_period * factor * (STABLE + 1)..base_period * factor * (STABLE + 2)]
+                && per_heights
+                    [base_period * factor * (STABLE + 1)..base_period * factor * (STABLE + 2)]
+                    == per_heights
+                        [base_period * factor * (STABLE + 2)..base_period * factor * (STABLE + 3)]
+                && heights[base_period * factor * (STABLE + 2)]
+                    - heights[base_period * factor * (STABLE + 1)]
+                    == heights[base_period * factor * (STABLE + 1)]
+                        - heights[base_period * factor * STABLE]
+            {
+                best_factor = Some(factor);
+                break;
+            }
+        }
+        let best_factor = best_factor.unwrap();
+
+        const GOAL: usize = 1000000000000 - 1; // -1 to go to 0-index
+
+        let offset = GOAL % (base_period * best_factor);
+        let coeff = GOAL / (base_period * best_factor);
+        let res = heights[base_period * best_factor * STABLE + offset]
+            + (heights[base_period * best_factor * (STABLE + 1)]
+                - heights[base_period * best_factor * STABLE])
+                * (coeff - STABLE);
+        res
     }
 }
 
@@ -159,4 +125,90 @@ fn dbg_state(state: &Vec<Vec<bool>>) {
         println!()
     }
     println!()
+}
+
+#[inline]
+fn solve_part1(input: &[i32], steps: usize, mut heights: Option<&mut Vec<usize>>) -> usize {
+    let blocks = vec![
+        vec![
+            [true, false, false, false],
+            [true, false, false, false],
+            [true, false, false, false],
+            [true, false, false, false],
+        ], // horizontal line
+        vec![
+            [false, true, false, false],
+            [true, true, true, false],
+            [false, true, false, false],
+        ], // cross
+        vec![
+            [true, false, false, false],
+            [true, false, false, false],
+            [true, true, true, false],
+        ], // reverse L
+        vec![[true, true, true, true]], // vertical line
+        vec![[true, true, false, false], [true, true, false, false]], // square
+    ];
+
+    let mut wind = Repeater::new(input);
+    let mut current_state = vec![Vec::with_capacity(steps * 4); 7];
+
+    for b in 0..steps {
+        let current_block = &blocks[b % blocks.len()];
+        let mut height = current_state.iter().map(Vec::len).max().unwrap() + 3;
+        let mut xoff = 2;
+
+        let mut step_state = true;
+
+        loop {
+            let new_xoff;
+            let new_height;
+            let mut bottom = false;
+            if step_state {
+                new_xoff = (xoff + wind.next()).clamp(0, 7 - current_block.len() as i32);
+                new_height = height;
+            } else {
+                if height > 0 {
+                    new_height = height - 1;
+                } else {
+                    new_height = height;
+                    bottom = true;
+                }
+                new_xoff = xoff;
+            }
+
+            let mut valid = true; // allow detection of bottom
+            'main: for i in 0..current_block.len() {
+                for y in 0..4 {
+                    if current_block[i][y]
+                        && get(&current_state[new_xoff as usize + i], new_height + y)
+                    {
+                        valid = false;
+                        break 'main;
+                    }
+                }
+            }
+
+            if (!valid || bottom) && !step_state {
+                for i in 0..current_block.len() {
+                    for y in 0..4 {
+                        if current_block[i][y] {
+                            set(&mut current_state[xoff as usize + i], height + y);
+                        }
+                    }
+                }
+                break;
+            }
+            if valid {
+                height = new_height;
+                xoff = new_xoff;
+            }
+            step_state = !step_state;
+        }
+
+        if let Some(h) = &mut heights {
+            h.push(current_state.iter().map(Vec::len).max().unwrap());
+        }
+    }
+    current_state.iter().map(Vec::len).max().unwrap()
 }
