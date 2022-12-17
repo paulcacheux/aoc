437a83c05eac8d6748b0a745403d3b0118a35517
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-12-17 13:07:16 +0100

    part1 working

--- src/aoc2022/day17.rs
+++ src/aoc2022/day17.rs
@@ -20,60 +20,91 @@ impl ParseInput<Day17> for Aoc2022 {
 }
 
 impl Solution<Day17> for Aoc2022 {
-    type Part1Output = u32;
+    type Part1Output = usize;
     type Part2Output = u32;
 
-    fn part1(input: &Vec<i32>) -> u32 {
+    fn part1(input: &Vec<i32>) -> usize {
         let blocks = vec![
-            vec![(0, 1), (0, 1), (0, 1), (0, 1)], // horizontal line
-            vec![(1, 1), (0, 3), (1, 1)],         // cross
-            vec![(0, 1), (0, 1), (0, 3)],         // reverse L
-            vec![(0, 4)],                         // vertical line
-            vec![(0, 2), (0, 2)],                 // square
+            vec![
+                [true, false, false, false],
+                [true, false, false, false],
+                [true, false, false, false],
+                [true, false, false, false],
+            ], // horizontal line
+            vec![
+                [false, true, false, false],
+                [true, true, true, false],
+                [false, true, false, false],
+            ], // cross
+            vec![
+                [true, false, false, false],
+                [true, false, false, false],
+                [true, true, true, false],
+            ], // reverse L
+            vec![[true, true, true, true]], // vertical line
+            vec![[true, true, false, false], [true, true, false, false]], // square
         ];
 
+        const STEPS: usize = 2022;
         let mut wind = Repeater::new(input);
-        let mut current_state = [0; 7];
+        let mut current_state = vec![Vec::with_capacity(STEPS * 4); 7];
 
-        for b in 0..2022 {
+        for b in 0..STEPS {
             let current_block = &blocks[b % blocks.len()];
-            let mut height = current_state.iter().copied().max().unwrap() + 3;
+            let mut height = current_state.iter().map(Vec::len).max().unwrap() + 3;
             let mut xoff = 2;
 
-            let mut state = true;
+            let mut step_state = true;
 
             loop {
                 let new_xoff;
                 let new_height;
-                if state {
+                let mut bottom = false;
+                if step_state {
                     new_xoff = (xoff + wind.next()).clamp(0, 7 - current_block.len() as i32);
                     new_height = height;
                 } else {
-                    new_height = height - 1;
+                    if height > 0 {
+                        new_height = height - 1;
+                    } else {
+                        new_height = height;
+                        bottom = true;
+                    }
                     new_xoff = xoff;
                 }
-                state = !state;
 
-                let valid = current_state
-                    .iter()
-                    .skip(new_xoff as usize)
-                    .zip(current_block.iter().map(|p| p.0 + new_height))
-                    .all(|(s, y)| y >= *s);
+                let mut valid = true; // allow detection of bottom
+                'main: for i in 0..current_block.len() {
+                    for y in 0..4 {
+                        if current_block[i][y]
+                            && get(&current_state[new_xoff as usize + i], new_height + y)
+                        {
+                            valid = false;
+                            break 'main;
+                        }
+                    }
+                }
 
-                if !valid {
+                if (!valid || bottom) && !step_state {
                     for i in 0..current_block.len() {
-                        current_state[xoff as usize + i] =
-                            height + current_block[i].0 + current_block[i].1;
+                        for y in 0..4 {
+                            if current_block[i][y] {
+                                set(&mut current_state[xoff as usize + i], height + y);
+                            }
+                        }
                     }
                     break;
                 }
-                height = new_height;
-                xoff = new_xoff;
+                if valid {
+                    height = new_height;
+                    xoff = new_xoff;
+                }
+                step_state = !step_state;
             }
-            dbg!(current_state);
+            // dbg_state(&current_state);
         }
 
-        todo!()
+        current_state.iter().map(Vec::len).max().unwrap()
     }
 
     fn part2(_input: &Vec<i32>) -> u32 {
@@ -96,4 +127,36 @@ impl<'a, T: Copy> Repeater<'a, T> {
         self.index = (self.index + 1) % self.inner.len();
         val
     }
+}
+
+fn set(v: &mut Vec<bool>, index: usize) {
+    if index >= v.len() {
+        v.resize(index + 1, false);
+    }
+    v[index] = true;
+}
+
+fn get(v: &Vec<bool>, index: usize) -> bool {
+    if index >= v.len() {
+        false
+    } else {
+        v[index]
+    }
+}
+
+#[allow(dead_code)]
+fn dbg_state(state: &Vec<Vec<bool>>) {
+    let height = state.iter().map(Vec::len).max().unwrap();
+
+    for i in (0..height).rev() {
+        for col in state {
+            if get(col, i) {
+                print!("#")
+            } else {
+                print!(".")
+            }
+        }
+        println!()
+    }
+    println!()
 }
