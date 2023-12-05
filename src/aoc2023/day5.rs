c19a5059f9bf50828552c05688fd8998723072c5
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-12-05 11:01:47 +0100

    small cleanup

--- src/aoc2023/day5.rs
+++ src/aoc2023/day5.rs
@@ -55,9 +55,9 @@ impl ParseInput<Day5> for Aoc2023 {
         let mut current = Mapping::default();
         for line in lines_iter {
             if let Some(desc) = line.strip_suffix(" map:") {
-               let (from, to) = desc.split_once("-to-").unwrap();
-               current.from = from.to_owned();
-               current.to = to.to_owned();
+                let (from, to) = desc.split_once("-to-").unwrap();
+                current.from = from.to_owned();
+                current.to = to.to_owned();
             } else if line.trim().is_empty() {
                 mappings.push(current);
                 current = Mapping::default();
@@ -66,7 +66,11 @@ impl ParseInput<Day5> for Aoc2023 {
                 let destination = values.next().unwrap().parse().unwrap();
                 let source = values.next().unwrap().parse().unwrap();
                 let len = values.next().unwrap().parse().unwrap();
-                current.ranges.push(MappingRange { destination, source, len })
+                current.ranges.push(MappingRange {
+                    destination,
+                    source,
+                    len,
+                })
             }
         }
 
@@ -74,39 +78,30 @@ impl ParseInput<Day5> for Aoc2023 {
             mappings.push(current);
         }
 
-        Input {
-            seeds,
-            mappings,
-        }
+        Input { seeds, mappings }
     }
 }
 
 impl Solution<Day5> for Aoc2023 {
     type Part1Output = u64;
-    type Part2Output = u32;
+    type Part2Output = u64;
 
     fn part1(input: &Input) -> u64 {
-        let mut min = None;
-        for &seed in &input.seeds {
-            let mut current = seed;
-            for mapping in &input.mappings {
-                current = mapping.transform(current);
-            }
-
-            match min {
-                None => {
-                    min = Some(current);
+        input
+            .seeds
+            .iter()
+            .map(|&seed| {
+                let mut current = seed;
+                for mapping in &input.mappings {
+                    current = mapping.transform(current);
                 }
-                Some(m) if current < m => {
-                    min = Some(current);
-                }
-                _ => {}
-            }
-        }
-        min.unwrap()
+                current
+            })
+            .min()
+            .unwrap()
     }
 
-    fn part2(input: &Input) -> u32 {
+    fn part2(input: &Input) -> u64 {
         todo!()
     }
 }
