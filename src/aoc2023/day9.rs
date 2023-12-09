77cd6e28fd677a42b4b59748fecc2bbf3d0f4873
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-12-09 09:32:55 +0100

    and part2

--- src/aoc2023/day9.rs
+++ src/aoc2023/day9.rs
@@ -20,7 +20,7 @@ impl ParseInput<Day9> for Aoc2023 {
 
 impl Solution<Day9> for Aoc2023 {
     type Part1Output = i32;
-    type Part2Output = u32;
+    type Part2Output = i32;
 
     fn part1(input: &Vec<Vec<i32>>) -> i32 {
         let mut sum = 0;
@@ -52,7 +52,36 @@ impl Solution<Day9> for Aoc2023 {
         sum
     }
 
-    fn part2(_input: &Vec<Vec<i32>>) -> u32 {
-        todo!()
+    fn part2(input: &Vec<Vec<i32>>) -> i32 {
+        let mut sum = 0;
+        for line in input {
+            let mut history = Vec::new();
+            let mut current = line.clone();
+
+            while !current.iter().all(|&v| v == 0) {
+                history.push(current.clone());
+                let mut next_line: Vec<i32> = Vec::with_capacity(current.len() - 1);
+                for [a, b] in current.array_windows() {
+                    let delta = b - a;
+                    next_line.push(delta);
+                }
+                current = next_line;
+            }
+
+            for i in (0..history.len()).rev() {
+                let under_value = if i + 1 < history.len() {
+                    *history[i+1].first().unwrap()
+                } else {
+                    0
+                };
+                let new_value = history[i].first().unwrap() - under_value;
+
+                let mut new_line = vec![new_value];
+                new_line.extend_from_slice(&history[i]);
+                history[i] = new_line;
+            }
+            sum += history[0].first().unwrap();
+        }
+        sum
     }
 }
