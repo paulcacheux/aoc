adc420c7617e9a0a3ad2c5cf0da0c9f15d42948b
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-12-10 19:12:11 +0100

    base code

---
+++ src/aoc2023/day10.rs
@@ -0,0 +1,25 @@
+use crate::aoc2023::Aoc2023;
+use crate::traits::days::Day10;
+use crate::traits::ParseInput;
+use crate::traits::Solution;
+
+impl ParseInput<Day10> for Aoc2023 {
+    type Parsed = _;
+
+    fn parse_input(input: &str) -> Self::Parsed {
+        todo!()
+    }
+}
+
+impl Solution<Day10> for Aoc2023 {
+    type Part1Output = u32;
+    type Part2Output = u32;
+
+    fn part1(input: &_) -> u32 {
+        todo!()
+    }
+
+    fn part2(input: &_) -> u32 {
+        todo!()
+    }
+}
--- src/aoc2023/mod.rs
+++ src/aoc2023/mod.rs
@@ -12,6 +12,7 @@ pub mod day6;
 pub mod day7;
 pub mod day8;
 pub mod day9;
+pub mod day10;
 
 pub fn run_solution_for_day(day: u32, input: &str, results: Option<Results>) -> Option<TimingData> {
     let r = results
@@ -28,6 +29,7 @@ pub fn run_solution_for_day(day: u32, input: &str, results: Option<Results>) -> 
         7 => run::<Aoc2023, Day7>(input, r),
         8 => run::<Aoc2023, Day8>(input, r),
         9 => run::<Aoc2023, Day9>(input, r),
+        10 => run::<Aoc2023, Day10>(input, r),
         _ => return None,
     };
     Some(elapsed)
