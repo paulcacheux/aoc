6507fe7c47fc4f88accd102cda66c455da581a33
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-12-13 10:01:27 +0100

    common code for both parts

--- src/aoc2023/day13.rs
+++ src/aoc2023/day13.rs
@@ -26,90 +26,62 @@ impl Solution<Day13> for Aoc2023 {
     type Part2Output = usize;
 
     fn part1(input: &Vec<Grid<bool>>) -> usize {
-        let mut score = 0;
-        for entry in input {
-            // look for x-axis mirror
-            'search: for mx in 1..entry.width {
-                for x in 0..mx {
-                    let opposite_x: usize = mx + (mx - x - 1);
-                    if opposite_x >= entry.width {
-                        continue;
-                    }
-
-                    for y in 0..entry.height {
-                        if entry.get(x, y) != entry.get(opposite_x, y) {
-                            continue 'search;
-                        }
-                    }
-                }
-                score += mx;
-            }
-
-            // look for y-axis mirror
-            'search: for my in 1..entry.height {
-                for y in 0..my {
-                    let opposite_y: usize = my + (my - y - 1);
-                    if opposite_y >= entry.height {
-                        continue;
-                    }
-
-                    for x in 0..entry.width {
-                        if entry.get(x, y) != entry.get(x, opposite_y) {
-                            continue 'search;
-                        }
-                    }
-                }
-                score += 100 * my;
-            }
-        }
-        score
+        solve(input, 0)
     }
 
     fn part2(input: &Vec<Grid<bool>>) -> usize {
-        let mut score = 0;
-        for entry in input {
-            // look for x-axis mirror
-            for mx in 1..entry.width {
-                let mut errors = 0;
-                for x in 0..mx {
-                    let opposite_x: usize = mx + (mx - x - 1);
-                    if opposite_x >= entry.width {
-                        continue;
-                    }
+        solve(input, 1)
+    }
+}
 
-                    for y in 0..entry.height {
-                        if entry.get(x, y) != entry.get(opposite_x, y) {
-                            errors += 1;
-                        }
-                    }
+fn solve(input: &[Grid<bool>], expected_errors: usize) -> usize {
+    let mut score = 0;
+    for entry in input {
+        // look for x-axis mirror
+        'search: for mx in 1..entry.width {
+            let mut errors = 0;
+            for x in 0..mx {
+                let opposite_x: usize = mx + (mx - x - 1);
+                if opposite_x >= entry.width {
+                    continue;
                 }
 
-                if errors == 1 {
-                    score += mx;
+                for y in 0..entry.height {
+                    if entry.get(x, y) != entry.get(opposite_x, y) {
+                        if expected_errors == 0 {
+                            continue 'search;
+                        }
+                        errors += 1;
+                    }
                 }
             }
-
-            // look for y-axis mirror
-            for my in 1..entry.height {
-                let mut errors = 0;
-                for y in 0..my {
-                    let opposite_y: usize = my + (my - y - 1);
-                    if opposite_y >= entry.height {
-                        continue;
-                    }
-
-                    for x in 0..entry.width {
-                        if entry.get(x, y) != entry.get(x, opposite_y) {
-                            errors += 1;
-                        }
-                    }
-                }
-
-                if errors == 1 {
-                    score += 100 * my
-                }
+            if errors == expected_errors {
+                score += mx;
             }
         }
-        score
+
+        // look for y-axis mirror
+        'search: for my in 1..entry.height {
+            let mut errors = 0;
+            for y in 0..my {
+                let opposite_y: usize = my + (my - y - 1);
+                if opposite_y >= entry.height {
+                    continue;
+                }
+
+                for x in 0..entry.width {
+                    if entry.get(x, y) != entry.get(x, opposite_y) {
+                        if expected_errors == 0 {
+                            continue 'search;
+                        }
+                        errors += 1;
+                    }
+                }
+            }
+            if errors == expected_errors {
+                score += 100 * my;
+            }
+        }
     }
+    score
 }
