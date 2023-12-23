153329256ce88c8a7b941409823104ee15d5c7ab
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-12-23 17:41:33 +0100

    part1 with graph simplification

--- src/aoc2023/day23.rs
+++ src/aoc2023/day23.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 use crate::aoc2023::Aoc2023;
@@ -20,63 +21,85 @@ impl Solution<Day23> for Aoc2023 {
     type Part2Output = usize;
 
     fn part1(input: &Grid<char>) -> usize {
-        find_longest_path(input, true)
+        let edges = build_simplified_graph(input);
+
+        let start = (1, 0);
+        let end = (input.width - 2, input.height - 1);
+
+        let mut open_queue = vec![(0, start, HashSet::new())];
+        let mut longest = 0;
+
+        while let Some((distance, (x, y), mut visited)) = open_queue.pop() {
+            if (x, y) == end {
+                if distance > longest {
+                    longest = distance;
+                }
+            } else {
+                visited.insert((x, y));
+            }
+
+            if let Some(edges) = edges.get(&(x, y)) {
+                for edge in edges {
+                    if !visited.contains(&edge.to) {
+                        open_queue.push((distance + edge.distance, edge.to, visited.clone()));
+                    }
+                }
+            }
+        }
+
+        longest
     }
 
-    fn part2(input: &Grid<char>) -> usize {
-        find_longest_path(input, false)
+    fn part2(_input: &Grid<char>) -> usize {
+        todo!()
     }
 }
 
-fn find_longest_path(input: &Grid<char>, with_slopes: bool) -> usize {
+fn build_simplified_graph(input: &Grid<char>) -> HashMap<(usize, usize), HashSet<Edge>> {
     let start = (1, 0);
     let end = (input.width - 2, input.height - 1);
-    assert_eq!(*input.get(start.0, start.1), '.');
-    assert_eq!(*input.get(end.0, end.1), '.');
 
-    let mut open_queue = vec![(0, start, HashSet::new())];
-    let mut longest = 0;
+    let mut open_queue = vec![PathPart {
+        parent: start,
+        pos: start,
+        distance: 0,
+        visited: HashSet::new(),
+    }];
     let mut new_cells = Vec::with_capacity(4);
 
-    while let Some((distance, (x, y), mut visited)) = open_queue.pop() {
-        if (x, y) == end {
-            if distance > longest {
-                longest = distance;
-            }
-        } else {
-            visited.insert((x, y));
-        }
+    let mut graph: HashMap<(usize, usize), HashSet<Edge>> = HashMap::new();
 
-        let force_direction = if with_slopes {
-            match *input.get(x, y) {
-                '>' => Some(Direction::East),
-                '<' => Some(Direction::West),
-                'v' => Some(Direction::South),
-                _ => None,
-            }
-        } else {
-            None
+    while let Some(PathPart {
+        parent,
+        pos: (x, y),
+        distance,
+        mut visited,
+    }) = open_queue.pop()
+    {
+        visited.insert((x, y));
+
+        let force_direction = match *input.get(x, y) {
+            '>' => Some(Direction::East),
+            '<' => Some(Direction::West),
+            'v' => Some(Direction::South),
+            _ => None,
         };
 
         new_cells.clear();
 
         for (direction, nx, ny) in input.get_neighbors_with_direction(x, y) {
-            if with_slopes {
-                if let Some(fdir) = force_direction {
-                    if fdir != direction {
-                        continue;
-                    }
+            if let Some(fdir) = force_direction {
+                if fdir != direction {
+                    continue;
                 }
+            }
 
-                match *input.get(nx, ny) {
-                    '#' => continue,
-                    '>' if direction != Direction::East => continue,
-                    '<' if direction != Direction::West => continue,
-                    'v' if direction != Direction::South => continue,
-                    _ => {}
-                }
-            } else if *input.get(nx, ny) == '#' {
-                continue;
+            match *input.get(nx, ny) {
+                '#' => continue,
+                '>' if direction != Direction::East => continue,
+                '<' if direction != Direction::West => continue,
+                'v' if direction != Direction::South => continue,
+                _ => {}
             }
 
             if !visited.contains(&(nx, ny)) {
@@ -84,13 +107,43 @@ fn find_longest_path(input: &Grid<char>, with_slopes: bool) -> usize {
             }
         }
 
+        if new_cells.len() != 1 || (x, y) == end {
+            graph.entry(parent).or_default().insert(Edge {
+                to: (x, y),
+                distance,
+            });
+        }
+
         if new_cells.len() == 1 {
-                open_queue.push((distance + 1, new_cells[0], visited));
+            open_queue.push(PathPart {
+                parent,
+                pos: new_cells[0],
+                distance: distance + 1,
+                visited,
+            });
         } else {
-            for &cell in &new_cells {
-                open_queue.push((distance + 1, cell, visited.clone()));
+            for &pos in &new_cells {
+                open_queue.push(PathPart {
+                    parent: (x, y),
+                    pos,
+                    distance: 1,
+                    visited: visited.clone(),
+                });
             }
         }
     }
-    longest
+    graph
+}
+
+struct PathPart {
+    parent: (usize, usize),
+    pos: (usize, usize),
+    distance: usize,
+    visited: HashSet<(usize, usize)>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Edge {
+    to: (usize, usize),
+    distance: usize,
 }
