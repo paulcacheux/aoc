e6a3597cc4dad9068dceb192f0174759ff20e8a1
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-12-24 15:39:00 +0100

    part2

--- src/aoc2023/day24.rs
+++ src/aoc2023/day24.rs
@@ -1,3 +1,4 @@
+use nalgebra::matrix;
 use nalgebra::vector;
 
 use crate::aoc2023::Aoc2023;
@@ -6,11 +7,20 @@ use crate::traits::ParseInput;
 use crate::traits::Solution;
 
 pub type Vec3 = nalgebra::Vector3<f64>;
+pub type Vec6 = nalgebra::Vector6<f64>;
+pub type Mat3 = nalgebra::Matrix3<f64>;
+pub type Mat6 = nalgebra::Matrix6<f64>;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Ball {
     pos: Vec3,
     speed: Vec3,
+}
+
+impl Ball {
+    fn pos_cross_speed(&self) -> Vec3 {
+        self.pos.cross(&self.speed)
+    }
 }
 
 impl ParseInput<Day24> for Aoc2023 {
@@ -41,7 +51,7 @@ impl ParseInput<Day24> for Aoc2023 {
 
 impl Solution<Day24> for Aoc2023 {
     type Part1Output = usize;
-    type Part2Output = u32;
+    type Part2Output = u64;
 
     fn part1(input: &Vec<Ball>) -> usize {
         const MIN: f64 = 200000000000000.0;
@@ -71,7 +81,40 @@ impl Solution<Day24> for Aoc2023 {
         counter
     }
 
-    fn part2(_input: &Vec<Ball>) -> u32 {
-        todo!()
+    fn part2(input: &Vec<Ball>) -> u64 {
+        assert!(input.len() >= 3);
+
+        let mut rhs = Vec6::zeros();
+        let first_rows = input[1].pos_cross_speed() - input[0].pos_cross_speed();
+        let last_rows = input[2].pos_cross_speed() - input[0].pos_cross_speed();
+        rhs.fixed_rows_mut::<3>(0).copy_from(&first_rows);
+        rhs.fixed_rows_mut::<3>(3).copy_from(&last_rows);
+
+        let mut big_mat = Mat6::zeros();
+        big_mat
+            .fixed_view_mut::<3, 3>(0, 0)
+            .copy_from(&(cross_mat(input[0].speed) - cross_mat(input[1].speed)));
+        big_mat
+            .fixed_view_mut::<3, 3>(3, 0)
+            .copy_from(&(cross_mat(input[0].speed) - cross_mat(input[2].speed)));
+        big_mat
+            .fixed_view_mut::<3, 3>(0, 3)
+            .copy_from(&(cross_mat(input[1].pos) - cross_mat(input[0].pos)));
+        big_mat
+            .fixed_view_mut::<3, 3>(3, 3)
+            .copy_from(&(cross_mat(input[2].pos) - cross_mat(input[0].pos)));
+
+        let res = big_mat.try_inverse().unwrap() * rhs;
+
+        let mut sum = 0;
+        for i in 0..3 {
+            sum += res[i].round() as u64;
+        }
+
+        sum
     }
+}
+
+fn cross_mat(v: Vec3) -> Mat3 {
+    matrix![0.0, -v[2], v[1]; v[2], 0.0, -v[0]; -v[1], v[0], 0.0]
 }
