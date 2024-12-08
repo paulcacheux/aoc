a7d5112e9dde523e83fe6d50c4752d18747bdf59
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-12-08 13:45:34 +0100

    base day6

---
+++ src/aoc2024/day6.rs
@@ -0,0 +1,65 @@
+use crate::aoc2024::Aoc2024;
+use crate::grid::Grid;
+use crate::traits::days::Day6;
+use crate::traits::ParseInput;
+use crate::traits::Solution;
+
+impl ParseInput<Day6> for Aoc2024 {
+    type Parsed = (Grid<bool>, usize, usize);
+
+    fn parse_input(input: &str) -> Self::Parsed {
+        let mut sx = 0;
+        let mut sy = 0;
+        let grid = Grid::parse(input, |c| c);
+
+        let mut bgrid = Grid::new(grid.width, grid.height, false);
+        grid.iter().for_each(|(x, y, &c)| {
+            let val = match c {
+                '#' => true,
+                '.' => false,
+                '^' => {
+                    sx = x;
+                    sy = y;
+                    false
+                }
+                _ => unreachable!(),
+            };
+            bgrid.set(x, y, val);
+        });
+        (bgrid, sx, sy)
+    }
+}
+
+impl Solution<Day6> for Aoc2024 {
+    type Part1Output = u32;
+    type Part2Output = u32;
+
+    fn part1((grid, sx, sy): &(Grid<bool>, usize, usize)) -> u32 {
+        let (mut x, mut y) = (*sx, *sy);
+        let (mut dx, mut dy) = (0, -1);
+
+        loop {
+            let (nx, ny) = (x.offset(dx), y.offset(dy));
+        }
+    }
+
+    fn part2(_input: &(Grid<bool>, usize, usize)) -> u32 {
+        todo!()
+    }
+}
+
+fn offset_pair(grid: &Grid<bool>, x: usize, y: usize, dx: isize, dy: isize) -> Option<(usize, usize)> {
+    let nx = offset(x, dx, grid.width) else { return None };
+    let ny = offset(y, dy,grid.height) else { return None };
+    (nx, ny)
+}
+
+fn offset(base: usize, offset: isize, max: usize) -> Option<usize> {
+    let res = base as isize + offset;
+
+    if res < 0 || res >= max as isize {
+        return None;
+    }
+
+    Some(res as usize)
+}
